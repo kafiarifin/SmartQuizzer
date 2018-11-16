74f79980fef0687e0a59dfe257b03d74cb7a04f7
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-11-15 21:11:37 -0600

    Started Work on the Layout

--- src/components/quiz-wrapper/index.js
+++ src/components/quiz-wrapper/index.js
@@ -5,11 +5,26 @@ import './index.css';
 class QuizWrapper extends Component {
     render() {
         return (
-            <div className={'quizWrapper'}>
+            <div className="quizWrapper">
                 <div className="container">
                     <div className="row">
-                        <h3 className="col-6">Quiz Title</h3>
-                        <h3 className="col-6">Quiz Title</h3>
+                        <h3 className="col-6">Quiz Title Component</h3>
+                        <h3 className="col-6">Question Title Component</h3>
+                    </div>
+                    <div className="row">
+                        <h1 className="col-12">Progress Bar Component</h1>
+                    </div>
+                    <div className="row">
+                        <h1 className="col-12">Question Body Component</h1>
+                    </div>
+                    <div className="row">
+                        <h1 className="col-12">Multiple Choice Options Component</h1>
+                    </div>
+                    <div className="row">
+                        <h1 className="col-12">Explanation Component</h1>
+                    </div>
+                    <div className="row">
+                        <h1 className="col-12">Navigation Component</h1>
                     </div>
                 </div>
             </div>
