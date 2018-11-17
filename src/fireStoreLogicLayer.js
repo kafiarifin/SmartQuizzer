811590b90c4f5d0b227252c3ad2275909f887e64
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-11-17 15:46:25 -0600

    Setting up firestore

---
+++ src/fireStoreLogicLayer.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBHizhi9yWthb-uvNr_juILneAtp8bZmiQ",
+    authDomain: "tiberius-cougar-alpha.firebaseapp.com",
+    databaseURL: "https://tiberius-cougar-alpha.firebaseio.com",
+    projectId: "tiberius-cougar-alpha",
+    storageBucket: "tiberius-cougar-alpha.appspot.com",
+    messagingSenderId: "889412483336"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore().enablePersistence()
+    .catch(function (err) {
+        if (err.code === 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled
+            // in one tab at a a time.
+            // ...
+        } else if (err.code === 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+        }
+    });
+
+db.settings({
+    timestampsInSnapshots: true
+});
+
+export default db;
