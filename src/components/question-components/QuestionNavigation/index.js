e639c13f666fdb26acd7f410eed2bfe26e280ebc
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-11-17 18:27:25 -0600

    UI Updates

--- src/components/question-components/QuestionNavigation/index.js
+++ src/components/question-components/QuestionNavigation/index.js
@@ -4,12 +4,17 @@ import PropTypes from 'prop-types'
 export const QuestionNavigation = ({something}) => {
     return (
         <div  className="row m-3">
-            <div className="col-6">
+            <div className="col-7 text-left">
                 <button type="button" className="btn btn-success">Review</button>
             </div>
-            <div className="col-6">
+            <div className="col-3">
+                <button type="button" className="btn btn-outline-secondary disabled">
+                    Remaining Attempts <span className="badge badge-dark">2</span>
+                </button>
+            </div>
+            <div className="col-2 text-right">
                 <button type="button" className="btn btn-primary mx-1">Submit</button>
-                <button type="button" className="btn btn-primary">Next</button>
+                {/*<button type="button" className="btn btn-primary">Next</button>*/}
             </div>
         </div>
     )
