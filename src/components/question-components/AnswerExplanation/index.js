b7148e09384b8222f974c4ad27bde94ec65765c8
Scot Pfleghaar <scot.pfleghaar@bestbuy.com>
2020-06-10 10:45:09 -0500

    update

--- src/components/question-components/AnswerExplanation/index.js
+++ src/components/question-components/AnswerExplanation/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import ReactMarkdown from "react-markdown";
+import Speech from 'speak-tts'
 
 export const AnswerExplanation = ({answerExplanation, referenceImage}) => {
+
+    const handleClick = () => {
+        const speech = new Speech()
+        if (!speech.speaking()) {
+            speech.speak({
+                text: answerExplanation,
+            }).then(() => {
+                console.log("Success !")
+            }).catch(e => {
+                console.error("An error occurred :", e)
+            })
+        } else {
+            speech.cancel()
+        }
+    }
+
     return (
         <div className="row m-3">
             <div className="col-12">
@@ -10,7 +27,8 @@ export const AnswerExplanation = ({answerExplanation, referenceImage}) => {
                     <div className="card-body text-secondary text-left" role="alert">
                         {/*<ReactMarkdown escapeHtml={false}*/}
                         {/*               source={answerExplanation}/>*/}
-                        <p><span style={{'white-space': 'pre-line'}}>{answerExplanation}</span></p>
+                        <p onClick={handleClick}><span style={{'whiteSpace': 'pre-line'}}>{answerExplanation}</span></p>
+
                         {referenceImage && <img className="img-fluid" src={referenceImage} alt=""/>}
                     </div>
                 </div>
--- src/components/question-components/QuestionExplanation/index.js
+++ src/components/question-components/QuestionExplanation/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import ReactMarkdown from "react-markdown";
+import Speech from 'speak-tts'
 
 export const QuestionExplanation = ({complete, isIncorrect, explanation, wrongAnswer}) => {
+    const speech = new Speech()
+
+
+    const handleClick = () => {
+        const speech = new Speech()
+        if (!speech.speaking()) {
+            speech.speak({
+                text: explanation,
+            }).then(() => {
+                console.log("Success !")
+            }).catch(e => {
+                console.error("An error occurred :", e)
+            })
+        } else {
+            speech.cancel()
+        }
+    }
     return (
         <div className="row m-3">
             <div className="col-12">
@@ -12,7 +30,8 @@ export const QuestionExplanation = ({complete, isIncorrect, explanation, wrongAn
                     <div className="alert alert-primary" role="alert">
                         Correct!
                     </div> : null}
-                {wrongAnswer ? <div className="alert alert-danger" role="alert">Incorrect, Please Try Again</div> : null}
+                {wrongAnswer ?
+                    <div className="alert alert-danger" role="alert">Incorrect, Please Try Again</div> : null}
             </div>
             <div className="col-12 text-left">
                 {/*<h6>Consider:</h6>*/}
@@ -20,7 +39,7 @@ export const QuestionExplanation = ({complete, isIncorrect, explanation, wrongAn
             <div className="col-12 text-left">
                 {/*<ReactMarkdown escapeHtml={false}*/}
                 {/*source={explanation}/>*/}
-                <p>{explanation}</p>
+                <p onClick={handleClick}>{explanation}</p>
             </div>
         </div>
     )
