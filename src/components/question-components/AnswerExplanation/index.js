d9c19a0721aa067193a4f75fa6d50c390579f10d
Scot Pfleghaar <scot.pfleghaar@bestbuy.com>
2020-06-10 09:47:10 -0500

    update

--- src/components/question-components/AnswerExplanation/index.js
+++ src/components/question-components/AnswerExplanation/index.js
@@ -8,9 +8,9 @@ export const AnswerExplanation = ({answerExplanation, referenceImage}) => {
             <div className="col-12">
                 <div className="card border-secondary">
                     <div className="card-body text-secondary text-left" role="alert">
-                        <ReactMarkdown escapeHtml={false}
-                                       source={answerExplanation}/>
-                        {/*<p>{answerExplanation}</p>*/}
+                        {/*<ReactMarkdown escapeHtml={false}*/}
+                        {/*               source={answerExplanation}/>*/}
+                        <p><span style={{'white-space': 'pre-line'}}>{answerExplanation}</span></p>
                         {referenceImage && <img className="img-fluid" src={referenceImage} alt=""/>}
                     </div>
                 </div>
